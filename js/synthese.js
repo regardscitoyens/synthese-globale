3d40fd46eb6f5b35de5c9e17984a5cc986b39175
RouxRC <b.ooghe@gmail.com>
2015-03-28 02:13:58 +0100

    cleanup js

--- js/synthese.js
+++ js/synthese.js
@@ -7,10 +7,10 @@
 - handle current mps only?
 - by group?
 */
+(function (ns) {
 
-
-var deputes = {},
-  indicateurs = [
+  ns.deputes = {};
+  ns.indicateurs = [
     ["semaines_presence", "Semaines d'activité"],
     ["commission_presences", "Commissions &mdash; réunions"],
     ["commission_interventions", "Commissions &mdash; interventions"],
@@ -23,23 +23,24 @@ var deputes = {},
     ["propositions_signees", "Propositions de loi signées"],
     ["questions_orales", "Questions orales"],
     ["questions_ecrites", "Questions écrites"]
-  ],
-  start = [6, 12],
-  end = [(new Date()).getMonth() + 1, (new Date()).getFullYear() - 2000];
-  month = function(){
-    return "20" + start[1] + (String(start[0]).length < 2 ? "0" : "") + start[0];
-  },
-  timeout = 0,
-  download = function(m, last){
+  ];
+
+  ns.start = [6, 12];
+  ns.end = [(new Date()).getMonth() + 1, (new Date()).getFullYear() - 2000];
+  ns.timeout = 0;
+  ns.download = function(last) {
+    var m = "20" + ns.start[1] +
+            (String(ns.start[0]).length < 2 ? "0" : "") + ns.start[0];
     setTimeout(function(){
-      d3.json("http://www.nosdeputes.fr/synthese/" + m + "/json", function(error, data){
+      d3.json("http://www.nosdeputes.fr/synthese/"+m+"/json", function(e, data){
         data.deputes.forEach(function(d){
-          if (deputes[d.depute.id] == undefined) deputes[d.depute.id] = {};
+          if (ns.deputes[d.depute.id] == undefined)
+            ns.deputes[d.depute.id] = {};
           Object.keys(d.depute).forEach(function(k){
-            var i = parseInt(d.depute[k]);
             if (k != "id" && k != "nom" && k != "groupe") {
-              if (deputes[d.depute.id][k] == undefined) deputes[d.depute.id][k] = 0;
-              deputes[d.depute.id][k] += i; 
+              if (ns.deputes[d.depute.id][k] == undefined)
+                ns.deputes[d.depute.id][k] = 0;
+              ns.deputes[d.depute.id][k] += parseInt(d.depute[k]); 
             }
           });
         });
@@ -48,50 +49,62 @@ var deputes = {},
           document.getElementById("menu").style.display = "";
         }
       });
-    }, timeout);
-    timeout += 110;
-  },
-  displayTop = function(){
+    }, ns.timeout);
+    ns.timeout += 110;
+  };
+  
+  ns.displayTop = function() {
     var selec = document.getElementById("deputes"),
       sel = selec.options[selec.selectedIndex].value;
     d3.select("#data").html("").append("ul")
       .selectAll("li")
-      .data(indicateurs)
+      .data(ns.indicateurs)
       .enter().append("li")
       .html(function(d) {
-        return d[1] + " : <span>" + deputes[sel][d[0]] + "</span>";
+        return d[1] + " : <span>" + ns.deputes[sel][d[0]] + "</span>";
       });
-    d3.select("#photo").html('<img src="' + deputes[sel].photo + '" alt="' + deputes[sel].nom + '" title="' + deputes[sel].nom + '"/>');
+    d3.select("#photo").html(
+      '<img src="' + ns.deputes[sel].photo + '"' +
+          ' alt="' + deputes[sel].nom + '" title="' + deputes[sel].nom + '"/>'
+    );
   };
 
-while (start[0] != end[0] || start[1] != end[1]) {
-  download(month());
-  if (start[0] == 12) {
-    start = [1, start[1]+1];
-  } else start[0]++;
-}
-download(month(), true);
-
-d3.json("http://www.nosdeputes.fr/deputes/json", function(error, data){
-  var select = d3.select("#menu").append("select")
-    .attr("id", "deputes")
-    .on("change", displayTop)
-    .selectAll('option')
-    .data(data.deputes.sort(function(a,b){
-      return d3.ascending(a.depute.nom_de_famille, b.depute.nom_de_famille);
-    }))
-    .enter().append("option")
-    .attr("value", function(d) {
-      return d.depute.id;
-    })
-    .text(function(d) {
-      return d.depute.nom_de_famille + ' ' + d.depute.prenom + ' (' + d.depute.groupe_sigle + ')';
+  ns.init = function() {
+  
+    while (ns.start[0] != ns.end[0] || ns.start[1] != ns.end[1]) {
+      ns.download();
+      if (ns.start[0] == 12) {
+        ns.start = [1, ns.start[1]+1];
+      } else ns.start[0]++;
+    }
+    ns.download(true);
+  
+    d3.json("http://www.nosdeputes.fr/deputes/json", function(error, data){
+      var select = d3.select("#menu").append("select")
+        .attr("id", "deputes")
+        .on("change", ns.displayTop)
+        .selectAll('option')
+        .data(data.deputes.sort(function(a, b){
+          return d3.ascending(a.depute.nom_de_famille, b.depute.nom_de_famille);
+        }))
+        .enter().append("option")
+        .attr("value", function(d) {
+          return d.depute.id;
+        })
+        .text(function(d) {
+          return d.depute.nom_de_famille + ' ' + d.depute.prenom +
+                 ' (' + d.depute.groupe_sigle + ')';
+        });
+      data.deputes.forEach(function(d){
+        if (ns.deputes[d.depute.id] == undefined)
+          ns.deputes[d.depute.id] = d.depute;
+        else for (var key in d.depute)
+          ns.deputes[d.depute.id][key] = d.depute[key];
+        ns.deputes[d.depute.id].photo = (d.depute.url_nosdeputes + '/110')
+          .replace('.fr/', '.fr/depute/photo/');
+      });
     });
-  data.deputes.forEach(function(d){
-    if (deputes[d.depute.id] == undefined) deputes[d.depute.id] = d.depute;
-    else for (var key in d.depute)
-      deputes[d.depute.id][key] = d.depute[attrname];
-    deputes[d.depute.id].photo = d.depute.url_nosdeputes.replace('.fr/', '.fr/depute/photo/') + '/110';
-  });
-});
 
+  }();
+  
+})(window.synthese = window.synthese || {});
